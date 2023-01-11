4fb7eaf0bae84bbfb51cedad1633c74f2fe44299
Kozou <louisfloquey@gmail.com>
2023-01-11 16:07:02 +0100

    add more methods for GET request

--- CalculatorApp_frontend/src/services/OperationService.js
+++ CalculatorApp_frontend/src/services/OperationService.js
@@ -1,15 +1,26 @@
 import axios from "axios";
 
-const OPERATION_API_BASE_URL = 'http://localhost:8080/api/v1/operations';
+const OPERATION_API_BASE_URL = "http://localhost:8080/api/v1/operations";
 
 class OperationService {
-    getHistory() {
-        return axios.get(`${OPERATION_API_BASE_URL}/history`);
-    }
-    setOperation(data) {
-        console.log(data);
-        return axios.post(`${OPERATION_API_BASE_URL}/calcule`, data);
-    }
+  getHistory() {
+    return axios.get(`${OPERATION_API_BASE_URL}/history`);
+  }
+  getAddition() {
+    return axios.get(`${OPERATION_API_BASE_URL}/additions`);
+  }
+  getSoustraction() {
+    return axios.get(`${OPERATION_API_BASE_URL}/soustractions`);
+  }
+  getMultiplication() {
+    return axios.get(`${OPERATION_API_BASE_URL}/multiplications`);
+  }
+  getDivision() {
+    return axios.get(`${OPERATION_API_BASE_URL}/divisions`);
+  }
+  setOperation(data) {
+    return axios.post(`${OPERATION_API_BASE_URL}/calcule`, data);
+  }
 }
 
 export default new OperationService();
