9fe1676393ca8b2b924985dedd9d0e5cace1efbe
kozou4ever <contact@kozou.dev>
2023-01-12 11:59:09 +0100

    add filter buttons

--- CalculatorApp_frontend/src/components/DropdownList.vue
+++ CalculatorApp_frontend/src/components/DropdownList.vue
@@ -1,7 +1,11 @@
 <script setup>
 import { ref } from 'vue';
-import FilterIcon from './Icons/FilterIcon.vue'
-import TriangleIcon from './Icons/TriangleIcon.vue'
+import FilterIcon from './Icons/FilterIcon.vue';
+import TriangleIcon from './Icons/TriangleIcon.vue';
+import { useHistoryStore } from '../stores/displayHistory';
+
+const store = useHistoryStore();
+const { getAddition, getSoustraction, getMultiplication, getDivision, getHistory } = store
 
 var hidden = ref(true);
 const toggleHidden = () => {
@@ -11,29 +15,33 @@ const toggleHidden = () => {
 
 <template>
     <div class="flex flex-col items-end w-full pr-4 mb-4">
-        <button @click="toggleHidden()" id="dropdownDefaultButton" data-dropdown-toggle="dropdown"
-            class="text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-700 font-medium rounded-lg px-4 py-2.5 text-center inline-flex items-center text-xs w-24"
-            type="button">
-            <FilterIcon class="mr-2"></FilterIcon> Filtrer <TriangleIcon :class="hidden ? '' : 'rotate-180'">
-            </TriangleIcon>
-        </button>
-        <!-- Dropdown menu -->
-        <div id="dropdown" class="z-10 bg- rounded shadow w-24 bg-gray-700" :class="hidden ? 'hidden' : ''">
-            <ul class="py-1 text-xs text text-gray-200 text-center"
-                aria-labelledby="dropdownDefaultButton">
-                <li>
-                    <button class="block px-4 py-2 hover:bg-gray-800 w-full">Addition</button>
-                </li>
-                <li>
-                    <button class="block px-4 py-2 hover:bg-gray-800 w-full">Soustraction</button>
-                </li>
-                <li>
-                    <button class="block px-4 py-2 hover:bg-gray-800 w-full">Multiplication</button>
-                </li>
-                <li>
-                    <button class="block px-4 py-2 hover:bg-gray-800 w-full">Division</button>
-                </li>
-            </ul>
+        <div>
+            <button @click="toggleHidden()"
+                class="text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-700 font-medium rounded-lg px-4 py-2.5 text-center inline-flex items-center text-xs w-24"
+                type="button">
+                <FilterIcon class="mr-2"></FilterIcon> Filtrer <TriangleIcon :class="hidden ? '' : 'rotate-180'">
+                </TriangleIcon>
+            </button>
+            <!-- Dropdown menu -->
+            <div class="z-10 rounded absolute shadow w-24 bg-gray-700" :class="hidden ? 'hidden' : ''">
+                <ul class="py-1 text-xs text text-gray-200 text-center">
+                    <li>
+                        <button @click="getAddition()" class="block px-4 py-2 hover:bg-gray-800 w-full">Addition</button>
+                    </li>
+                    <li>
+                        <button @click="getSoustraction()" class="block px-4 py-2 hover:bg-gray-800 w-full">Soustraction</button>
+                    </li>
+                    <li>
+                        <button @click="getMultiplication()" class="block px-4 py-2 hover:bg-gray-800 w-full">Multiplication</button>
+                    </li>
+                    <li>
+                        <button @click="getDivision()" class="block px-4 py-2 hover:bg-gray-800 w-full">Division</button>
+                    </li>
+                    <li>
+                        <button @click="getHistory()" class="block px-4 py-2 hover:bg-gray-800 w-full">Tous</button>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 
--- CalculatorApp_frontend/src/views/HistoriqueView.vue
+++ CalculatorApp_frontend/src/views/HistoriqueView.vue
@@ -1,26 +1,24 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import OperationService from '../services/OperationService.js';
 import AnOperation from '../components/Operations/OperationHistory.vue';
 import DropdownList from '../components/DropdownList.vue';
+import { useHistoryStore } from '../stores/displayHistory';
+import { storeToRefs } from 'pinia';
+import { onMounted } from 'vue';
 
-const operations = ref([])
-
-const getHistory = () => {
-    OperationService.getHistory().then((res) => {
-        operations.value = res.data;
-    })
-}
+const store = useHistoryStore();
+const { getHistory } = store;
 
 onMounted(() => {
     getHistory();
-})
+});
+
+var { typeHistory } = storeToRefs(store);
 </script>
 
 <template>
     <DropdownList></DropdownList>
     <div class="flex flex-col w-full h-96 overflow-y-scroll" id="scroll">
-        <div v-for="operation in operations" :key="operation.id">
+        <div v-for="operation in typeHistory" :key="operation.id">
             <AnOperation :firstNumber="operation.first" :secondNumber="operation.second"
                 :operation="operation.operation" :result="operation.result"></AnOperation>
         </div>
